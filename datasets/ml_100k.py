c3ecd02df1a4cdaa699694bb0a48a118bd0abe32
deipss <deipss@qq.com>
2021-04-26 14:36:05 +0800

    100K

--- datasets/ml_100k.py
+++ datasets/ml_100k.py
@@ -12,22 +12,23 @@ class ML1MDataset(AbstractDataset):
 
     @classmethod
     def url(cls):
-        return 'http://files.grouplens.org/datasets/movielens/ml-1m.zip'
+        return 'http://files.grouplens.org/datasets/movielens/ml-100k.zip'
 
     @classmethod
     def zip_file_content_is_folder(cls):
         return True
 
     @classmethod
-    def all_raw_file_names(cls):
-        return ['movies.dat',
-                'ratings.dat',
-                'users.dat']
+    def all_raw_file_names(cs):
+        return [
+                'u.user',
+                'u.item',
+                'u.data']
 
     def load_ratings_df(self):
         folder_path = self._get_rawdata_folder_path()
-        file_path = folder_path.joinpath('ratings.dat')
-        df = pd.read_csv(file_path, sep='::', header=None)
+        file_path = folder_path.joinpath('u.data')
+        df = pd.read_csv(file_path, sep='\t', header=None)
         df.columns = ['uid', 'sid', 'rating', 'timestamp']
         return df
 
