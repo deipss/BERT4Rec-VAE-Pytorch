93f70aea0b62f4da6c2ae45664ec892fde9c78d8
deipss <deipss@qq.com>
2021-04-28 23:07:27 +0800

    新增 bert_bilstm

--- models/bert_bilstm.py
+++ models/bert_bilstm.py
@@ -8,12 +8,12 @@ class BERTBILSTMModel(BaseModel):
     def __init__(self, args):
         super().__init__(args)
         self.bert = BERT(args)
-        nn.LSTM(input_size=self.bert.hidden,
-                hidden_size=self.bert.hidden,
-                num_layers=self.bert.bilstm_num,
-                dropout=0.1,
-                batch_first=True,
-                bidirectional=True)
+        self.bilstm = nn.LSTM(input_size=self.bert.hidden,
+                              hidden_size=self.bert.hidden,
+                              num_layers=self.bert.bilstm_num,
+                              dropout=0.1,
+                              batch_first=True,
+                              bidirectional=True)
         self.out = nn.Linear(self.bert.hidden, args.num_items + 1)
 
     @classmethod
@@ -22,4 +22,5 @@ class BERTBILSTMModel(BaseModel):
 
     def forward(self, x):
         x = self.bert(x)
+        x = self.bilstm(x)
         return self.out(x)
