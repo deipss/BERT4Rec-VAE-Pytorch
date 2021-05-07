8e3c0af75a5429dd9a4f6f20666c22b267bfcd7f
deipss <deipss@qq.com>
2021-05-07 13:11:21 +0800

    bert_cnn code modify

--- models/bert_cnn.py
+++ models/bert_cnn.py
@@ -12,15 +12,14 @@ class BERTCNNModel(BaseModel):
                              out_channels=args.bert_max_len,
                              kernel_size=args.kernel_size,
                              stride=args.stride)
-        lout_size = (args.bert_hidden_units-(args.kernel_size-1)-1)//args.stride+1
+        lout_size = (args.bert_hidden_units - (args.kernel_size - 1) - 1) // args.stride + 1
         self.out = nn.Linear(lout_size, args.num_items + 1)
-
 
     @classmethod
     def code(cls):
         return 'bert_cnn'
 
-    def forward(self, x):
-        x = self.bert(x)
+    def forward(self, x, y):
+        x = self.bert(x, y)
         x = self.cnn(x)
         return self.out(x)
