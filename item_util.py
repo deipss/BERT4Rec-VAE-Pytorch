c543d9dca876410f92feb071af54f709ae6a0af0
deipss <deipss@qq.com>
2021-05-11 20:35:52 +0800

    推荐结果分析

--- item_util.py
+++ item_util.py
@@ -21,7 +21,9 @@ def get_E():
     model = model_factory(args)
     trainer = trainer_factory(args, model, train_loader, val_loader, test_loader, export_root)
     # trainer.train()
-    best_model = torch.load(os.path.join('/home/deipss/BERT4Rec-VAE-Pytorch-master/experiments/test_bert_128_ml-1m_2021-05-11_0_meta/models/best_acc_model.pth')).get('model_state_dict')
+    best_model = torch.load(os.path.join(
+        '/home/deipss/BERT4Rec-VAE-Pytorch-master/experiments/test_bert_128_ml-1m_2021-05-11_0_meta/models/best_acc_model.pth')).get(
+        'model_state_dict')
     model.load_state_dict(best_model)
     model.eval()
     global E
@@ -39,6 +41,7 @@ def to_device(d):
 
 def evaluate(emb, top_k, movie_lists):
     rst = []
+    cos_v = []
     for m in movie_lists:
         c = to_device(torch.LongTensor([m]))
         cos_result = F.cosine_similarity(emb[c], emb).sort(descending=True)
@@ -46,8 +49,11 @@ def evaluate(emb, top_k, movie_lists):
         cos_dis = cos_result[0]  # 分数
         rank_list = cos_dis_idx.data.cpu().numpy()[:top_k]  # 前K个最近距离的项目
         rank_list = rank_list.tolist()  # numpy array 转成 python list
+        rank_list_cos = cos_dis.data.cpu().numpy()[:top_k]  # 前K个最近距离的项目
+        rank_list_cos = rank_list_cos.tolist()  # numpy array 转成 python list
         rst.append(rank_list)
-    return rst
+        cos_v.append(rank_list_cos)
+    return rst, cos_v
 
 
 def search_neighbor_item():
@@ -57,13 +63,13 @@ def search_neighbor_item():
     # # 加载模型
     get_E()
     m_list = [mid2idx[i] for i in m_list]
-    rst = evaluate(E,11,m_list)
+    rst, cos_v = evaluate(E, 11, m_list)
 
     print(rst)
-    for i in rst:
-        print('emb_id=%d\tmovie_id=%d' % (i[0],midx2id[i[0]]))
-        print(get_info_by_sid(df,midx2id[i[0]]))
-        for k in i[1:]:
+    for i, v in zip(rst, cos_v):
+        print('emb_id=%d\tmovie_id=%d' % (i[0], midx2id[i[0]]))
+        print(get_info_by_sid(df, midx2id[i[0]]))
+        for k, c in zip(i[1:], v[1:]):
             if k not in midx2id.keys():
                 continue
             item_mid = midx2id[k]
@@ -78,10 +84,13 @@ def generate_meta_map():
 
 
 def get_info_by_sid(df, sid):
-    return df.loc[df['sid'] == sid].values.tolist()
+    info = df.loc[df['sid'] == sid].values.tolist()[0]
+    return '' + str(info[0]) + '\t' + str(info[1]) + '\t' + str(info[2])
 
 
 if __name__ == '__main__':
+    # df = generate_meta_map()
+    # get_info_by_sid(df, 12)
     args.mode = 'train'
     i = 128
     args.bert_hidden_units = i
