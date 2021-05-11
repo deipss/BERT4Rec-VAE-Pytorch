18f5832c05e2e7bdb63b4868426eab91f6e879af
deipss <deipss@qq.com>
2021-05-11 20:05:45 +0800

    推荐结果分析

--- item_util.py
+++ item_util.py
@@ -21,14 +21,14 @@ def get_E():
     model = model_factory(args)
     trainer = trainer_factory(args, model, train_loader, val_loader, test_loader, export_root)
     # trainer.train()
-    best_model = torch.load(os.path.join('/home/deipss/BERT4Rec-VAE-Pytorch-master/experiments/test_bert_128_ml-1m_2021-05-10_0/models/best_acc_model.pth')).get('model_state_dict')
+    best_model = torch.load(os.path.join('/home/deipss/BERT4Rec-VAE-Pytorch-master/experiments/test_bert_128_ml-1m_2021-05-10_0_bert/models/best_acc_model.pth')).get('model_state_dict')
     model.load_state_dict(best_model)
     model.eval()
     global E
     global mid2idx
     global midx2id
     mid2idx = dataloader.smap
-    midx2id = {i: s for i, s in mid2idx.items()}
+    midx2id = {i: s for s, i in mid2idx.items()}
     E = model.bert.embedding.token.weight.data
 
 
@@ -54,16 +54,18 @@ def search_neighbor_item():
     # 读取pandas
     df = generate_meta_map()
     m_list = [12, 17, 71, 177, 36, 23, 76, 44, 98, 337]
-    # 加载模型
+    # # 加载模型
     get_E()
+    m_list = [mid2idx[i] for i in m_list]
     rst = evaluate(E,11,m_list)
 
     print(rst)
     for i in rst:
-        target = midx2id[i[0]]
-        print('idx=%d=  mid=%d' % (i[0],midx2id[i[0]]))
-        print(get_info_by_sid(df,target))
+        print('emb_id=%d\tmovie_id=%d' % (i[0],midx2id[i[0]]))
+        print(get_info_by_sid(df,midx2id[i[0]]))
         for k in i[1:]:
+            if k not in midx2id.keys():
+                continue
             item_mid = midx2id[k]
             print(get_info_by_sid(df, item_mid))
 
