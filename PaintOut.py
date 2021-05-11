f57601c1c5bd8758bd181c5f126eccdfc7844b35
deipss <deipss@qq.com>
2021-05-11 19:44:10 +0800

    消融实验打印

--- PaintOut.py
+++ PaintOut.py
@@ -46,6 +46,7 @@ def data_load_bert_cnn():
                     data = json.load(data_file)
                     data_file = open(path + '/' + 'config.json')
                     data_config = json.load(data_file)
+                    # if data_config['blocks_1m_test'] == False:
                     if data_config['num_epochs'] != 80:
                         continue
                     print('%12s\t%3s\t%10s\t%d\t%d\t%s'
