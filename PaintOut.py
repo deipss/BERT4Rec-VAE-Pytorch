687ee29e0c395b78982ead6289013e1632058fc6
deipss <deipss@qq.com>
2021-05-26 21:28:53 +0800

    paintOut.py

--- PaintOut.py
+++ PaintOut.py
@@ -40,19 +40,25 @@ def data_load_bert_cnn():
     for root, dirs, files in os.walk(dir):
         for sub_dir in dirs:
             path = root + '/' + sub_dir
-            if 1 ==1  and '25_' in sub_dir:
+            if 1 ==1 :
                 try:
                     data, data_config = {}, {}
                     data_file = open(path + '/logs/' + 'test_metrics.json')
                     data = json.load(data_file)
                     data_file = open(path + '/' + 'config.json')
                     data_config = json.load(data_file)
-                    if not data_config['dim'] in [8,16]:
-                        continue
 
-                    print('%12s\t%3s\t%10s\t%d\t%d\t%s'
-                          % (data_config['model_code'], data_config['dim'], data_config['dataset_code'],
-                             data_config['bert_num_blocks'], data_config['bert_num_heads'], data_to_line(data)))
+
+                    print('%12s\t%3s\t%10s\t%d\t%d\t%d\t%d\t%d\t%s'
+                          % (data_config['model_code'],
+                             data_config['dim'],
+                             data_config['dataset_code'],
+                             data_config['bert_num_blocks'],
+                             data_config['bert_num_heads'],
+                             data_config['num_epochs'],
+                             data_config['stride'],
+                             data_config['kernel_size'],
+                             data_to_line(data)))
                 except BaseException:
                     pass
                     # print()
