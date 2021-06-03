14561f8cc4d3d9fab4dc9a3222dd4ac68d145719
deipss <deipss@qq.com>
2021-06-03 16:01:44 +0800

    behavior数据集处理

--- datasets/behavior.py
+++ datasets/behavior.py
@@ -26,7 +26,24 @@ class BehaviorDataset(AbstractDataset):
         folder_path = self._get_rawdata_folder_path()
         file_path = folder_path.joinpath('UserBehavior.csv')
         df = pd.read_csv(file_path, sep='::', header=None)
-        df.columns = ['uid', 'sid', 'category','behavior', 'timestamp']
+        df.columns = ['uid', 'sid', 'smeta','behavior', 'timestamp']
         return df
 
 
+    def generate_meta_map(self, smap):
+        df = self.load_ratings_df()
+        del df['uid']
+        del df['behavior']
+        del df['timestamp']
+        meta_set = set()
+        meta_set.update(list(df['smeta'].values))
+        set_len = len(meta_set)
+        umap = {u: i + 1 for i, u in enumerate(meta_set)}
+        # 位运算
+        df['smeta'] = df['smeta'].map(lambda x: [umap[x]])
+        df['smeta'] = df['smeta'].map(lambda x: x + [0] * (set_len - len(x)))
+        set_smap = smap.keys()
+        df['sid'] = df['sid'].map(lambda x: smap[x] if x in set_smap else x)
+
+        map = df.set_index('sid').T.to_dict('int')
+        return map['smeta'],len(meta_set)
