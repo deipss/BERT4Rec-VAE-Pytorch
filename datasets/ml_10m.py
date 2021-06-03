5008ca8f80b39031a5f08eafbeb8a7c99b3b2f8d
deipss <deipss@qq.com>
2021-06-03 15:36:42 +0800

    ml-10m meta-bert4rec 数据集处理

--- datasets/ml_10m.py
+++ datasets/ml_10m.py
@@ -31,3 +31,23 @@ class ML10MDataset(AbstractDataset):
         return df
 
 
+    def generate_meta_map(self, smap):
+        folder_path = self._get_rawdata_folder_path()
+        file_path = folder_path.joinpath('movies.dat')
+        df = pd.read_csv(file_path, sep='::', header=None)
+        df.columns = ['sid', 'sname', 'smeta']
+        df['smeta'] = df['smeta'].map(lambda x: x.split("|"))
+        meta_set = set()
+        for i in df['smeta'].values:
+            meta_set.update(i)
+        del df['sname']
+        set_len = len(meta_set)
+        umap = {u: i+1 for i, u in enumerate(meta_set)}
+        # 位运算
+        df['smeta'] = df['smeta'].map(lambda x: [umap[v] for v in x])
+        df['smeta'] = df['smeta'].map(lambda x: x + [0] * (set_len - len(x)))
+        set_smap = smap.keys()
+        df['sid'] = df['sid'].map(lambda x: smap[x] if x in set_smap else x)
+
+        map = df.set_index('sid').T.to_dict('int')
+        return map['smeta'],len(meta_set)
